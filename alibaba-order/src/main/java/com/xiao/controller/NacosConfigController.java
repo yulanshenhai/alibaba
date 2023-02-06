966f1055eb5f8c70332349213ca89bf56f20fb97
yulanshenhai <3360851236@qq.com>
2023-02-06 18:29:40 +0800

    alibaba

--- alibaba-order/src/main/java/com/xiao/controller/NacosConfigController.java
+++ alibaba-order/src/main/java/com/xiao/controller/NacosConfigController.java
@@ -1,25 +1,25 @@
-package com.xiao.controller;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @author xiao
- * <p> `@RefreshScope`: 启用动态刷新Nacos配置文件内容的功能
- */
-@RefreshScope
-@RestController
-@RequestMapping("/api/v1/nacos-config")
-public class NacosConfigController {
-
-    @Value("${project.author}")
-    private String author;
-
-    @GetMapping("/get-author")
-    public String getAuthor() {
-        return author;
-    }
-}
+//package com.xiao.controller;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * @author xiao
+// * <p> `@RefreshScope`: 启用动态刷新Nacos配置文件内容的功能
+// */
+//@RefreshScope
+//@RestController
+//@RequestMapping("/api/v1/nacos-config")
+//public class NacosConfigController {
+//
+//    @Value("${project.author}")
+//    private String author;
+//
+//    @GetMapping("/get-author")
+//    public String getAuthor() {
+//        return author;
+//    }
+//}

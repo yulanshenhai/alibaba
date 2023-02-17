70b54ed74a0021276a4100903ebde403c54295f1
yulanshenhai <3360851236@qq.com>
2023-02-17 12:14:39 +0800

    alibaba

--- 资源/v3_alibaba.sql
+++ 资源/v3_alibaba.sql
@@ -7,13 +7,15 @@ use alibaba;
 
 create table if not exists alibaba.user
 (
-    id          int(11) auto_increment comment '主键',
-    username    varchar(128) not null default '' comment '账号',
-    password    varchar(128) not null default '' comment '密码',
-    phone       char(11)     not null default '' comment '电话',
+    user_id          int(11) auto_increment comment '用户表主键',
+    username    varchar(128) not null default '' comment '登录账号',
+    password    varchar(128) not null default '' comment '登录密码',
+    phone       char(11)     not null default '' comment '用户电话',
+    nick_name   varchar(128) not null default '' comment '用户昵称',
+    real_name   varchar(128) not null default '' comment '用户真实姓名',
     create_time datetime     not null default now() comment '首次创建时间',
     last_modify datetime     not null default now() comment '最后修改时间',
-    primary key (id)
+    primary key (user_id)
 )
     comment '用户表';
 
@@ -43,16 +45,100 @@ create table if not exists alibaba.order
 )
     comment '订单表';
 
+create table if not exists alibaba.role
+(
+    role_id     int(11) auto_increment comment '角色表主键',
+    role_title  varchar(128) not null default '' comment '角色名称',
+    role_info   varchar(512) not null default '' comment '角色描述',
+    create_time datetime     not null default now() comment '首次创建时间',
+    last_modify datetime     not null default now() comment '最后修改时间',
+    primary key (role_id)
+)
+    comment '角色表';
+
+create table if not exists alibaba.permission
+(
+    permission_id    int(11) auto_increment comment '权限表主键',
+    permission_title varchar(128) not null default '' comment '权限名称',
+    permission_info  varchar(512) not null default '' comment '权限描述',
+    create_time      datetime     not null default now() comment '首次创建时间',
+    last_modify      datetime     not null default now() comment '最后修改时间',
+    primary key (permission_id)
+)
+    comment '权限表';
+
+create table if not exists alibaba.user_role
+(
+    user_role_id int(11) auto_increment comment '用户-角色中间表主键',
+    user_id      int(11) comment '用户表主键',
+    role_id        int(11) comment '角色表主键',
+    create_time    datetime not null default now() comment '首次创建时间',
+    last_modify    datetime not null default now() comment '最后修改时间',
+    primary key (user_role_id)
+)
+    comment '会员-角色中间表';
+
+create table if not exists alibaba.role_permission
+(
+    role_permission_id int(11) auto_increment comment '角色-权限中间表主键',
+    role_id            int(11) comment '角色表主键',
+    permission_id      int(11) comment '权限表主键',
+    create_time        datetime not null default now() comment '首次创建时间',
+    last_modify        datetime not null default now() comment '最后修改时间',
+    primary key (role_permission_id)
+)
+    comment '会员-角色中间表';
+
 # record
 
 insert into alibaba.user
-values (2, 'zhaosi', 'zhaosi', '18888888889', now(), now());
-# values (1, 'admin', 'admin', '18888888888', now(), now());
+values (1, 'zhaosi', 'zhaosi', '18211111111', '亚洲舞王', '赵国强', now(), now()),
+       (2, 'liuneng', 'liuneng', '18222222222', '象牙山小诸葛', '刘能', now(), now()),
+       (3, 'guangkun', 'guangkun', '18233333333', '首富', '赵国强', now(), now()),
+       (4, 'xuzhengli', 'xuzhengli', '18244444444', '村主任', '徐正利', now(), now());
+
+## product
 insert into alibaba.product
 values (1, '海尔冰箱', 3500.00, 5, now(), now()),
        (2, '小米电视', 4500.00, 6, now(), now()),
        (3, '华为电脑', 5500.00, 7, now(), now());
+
+## order
 insert into alibaba.order
 values (1, 'admin', 1, '海尔冰箱', 2, now(), now());
+
+## role
+insert into alibaba.role
+values (1, 'root', '超级用户', now(), now()),
+       (2, 'admin', '管理员', now(), now()),
+       (3, 'normal', '普通会员', now(), now());
+## permission
+insert into alibaba.permission
+values (1, 'insert', 'DML添加权限', now(), now()),
+       (2, 'select', 'DQL查询权限', now(), now()),
+       (3, 'update', 'DML修改权限', now(), now()),
+       (4, 'delete', 'DML删除权限', now(), now()),
+       (5, 'create', 'DDL创建权限', now(), now()),
+       (6, 'drop', 'DDL删除权限', now(), now());
+## member_role
+insert into alibaba.user_role
+values (1, 1, 1, now(), now()),
+       (2, 2, 2, now(), now()),
+       (3, 3, 3, now(), now()),
+       (4, 4, 2, now(), now()),
+       (5, 4, 3, now(), now());
+## role_permission
+insert into alibaba.role_permission
+values (1, 1, 5, now(), now()),
+       (2, 1, 6, now(), now()),
+       (3, 1, 3, now(), now()),
+       (4, 1, 4, now(), now()),
+       (5, 1, 5, now(), now()),
+       (6, 1, 6, now(), now()),
+       (7, 2, 1, now(), now()),
+       (8, 2, 2, now(), now()),
+       (9, 2, 3, now(), now()),
+       (10, 2, 4, now(), now()),
+       (11, 3, 2, now(), now());
 commit;
 
